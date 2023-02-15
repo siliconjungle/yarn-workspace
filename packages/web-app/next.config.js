91a896509c9b00d116d29de0973601a3541dffbd
James Addison <siliconjungle.io@gmail.com>
2023-02-15 16:24:11 +1100

    add multi zones

--- packages/web-app/next.config.js
+++ packages/web-app/next.config.js
@@ -1,6 +1,25 @@
+const DOCS_URL = 'http://localhost:3001'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   transpilePackages: ['@project/components'],
-};
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: `/:path*`,
+      },
+      {
+        source: '/docs',
+        destination: `${DOCS_URL}/docs`,
+      },
+      {
+        source: '/docs/:path*',
+        destination: `${DOCS_URL}/docs/:path*`,
+      },
+    ]
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
